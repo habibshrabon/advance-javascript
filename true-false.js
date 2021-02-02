f2ae90981d718a98dd08d184e9d621baf7ed3b21
unknown <snhabib777@gmail.com>
2021-02-02 10:48:12 +0600

    True vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//this is false
+// 0
+// ""
+// undefine
+// null
+// NaN
+
+//truthy
+// "0", " ", []
+
+
+const name = 0;//emty string hoyle false and 0 hoyle false otherwise string name and any num hoyle true. 
+
+if(name.length){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
