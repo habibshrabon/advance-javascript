08175fe8a481bdb8497a0634f68ce9c87f38b6fc
unknown <snhabib777@gmail.com>
2021-02-02 11:18:57 +0600

    Double vs Triple equals used

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,20 @@
+// //Double == is used
+const first = 2;
+const second = "2"; 
+if(first == second){
+    console.log("condition is true");
+}    
+else{
+    console.log("condition is false");
+}
+
+
+//Triple === is used
+const first = 2;
+const second = "2"; 
+if(first === second){
+    console.log("condition is true");
+}    
+else{
+    console.log("condition is false");
+}
